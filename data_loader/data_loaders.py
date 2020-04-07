8fa24a55e9efb9d7d61132624dd76c2724857287
Hoang Nghia Tuyen <hnt4499@gmail.com>
2020-04-08 01:06:33 +0800

    Add dataset utilities for DCGAN

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,20 +1,41 @@
 from torchvision import datasets, transforms
 
 from base import BaseDataLoader
+from . import datasets as custom_datasets
+from processing import pre_processing
 
 
-class MnistDataLoader(BaseDataLoader):
+class ImageNetLoader(BaseDataLoader):
+    """ImageNet data loader.
+
+    Parameters
+    ----------
+    data_dir : str
+        Root directory of the ImageNet dataset.
+    cls : str
+        A ImageNet class name, for example "n01531178", which corresponds to
+        "goldfinch".
+    include_val : bool
+        Whether to include images from validation set.
+    transform
+        A function to be applied to the input images. If `None`, the function
+        `DCGAN_transform` will be used with all default arguments
+        (`image_size=64`, `mean=0.0`, `std=1.0`).
+    batch_size : int
+        Mini-batch size for training.
+    shuffle : bool
+        Whether to shuffle the training set.
+    num_workers : int
+        The number of subprocesses to use for data loading. `0` means that the
+        data will be loaded in the main process. (default: `0`)
+
     """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0,
-                 num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
+    def __init__(self, data_dir, cls, include_val=False, transform=None,
+                 batch_size=128, shuffle=True, num_workers=1):
+        if transform is None:
+            transform = pre_processing.DCGAN_transform()
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training,
-                                      download=True, transform=trsfm)
+        self.dataset = custom_datasets.ImageNetDataset(
+            data_dir, cls, include_val=include_val, transform=transform)
         super().__init__(self.dataset, batch_size, shuffle,
-                         validation_split, num_workers)
+                         validation_split=0.0, num_workers=num_workers)
---
+++ data_loader/datasets.py
@@ -0,0 +1,83 @@
+import os
+
+import pandas as pd
+import torch
+from PIL import Image
+
+from processing import pre_processing
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning
+    # (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+def accimage_loader(path):
+    import accimage
+    try:
+        return accimage.Image(path)
+    except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        return pil_loader(path)
+
+
+def default_loader(path):
+    """Default image reader."""
+    from torchvision import get_image_backend
+    if get_image_backend() == 'accimage':
+        return accimage_loader(path)
+    else:
+        return pil_loader(path)
+
+
+class ImageNetDataset(torch.utils.data.Dataset):
+    """ImageNet dataset reader.
+
+    Parameters
+    ----------
+    root : str
+        Root directory of the ImageNet dataset.
+    cls : str
+        A ImageNet class name, for example "n01531178", which corresponds to
+        "goldfinch".
+    include_val : bool
+        Whether to include images from validation set.
+    transform
+        A function to be applied to the input images.
+
+    """
+    def __init__(self, root, cls, include_val=False, transform=None):
+        super(ImageNetDataset).__init__()
+        # Get train filepaths
+        train_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC/train")
+        train_dir = os.path.join(train_dir, cls)
+        train_names = os.listdir(train_dir)
+        train_paths = [os.path.join(train_dir, train_name) \
+            for train_name in train_names]
+        self.filepaths = train_paths
+        # Get validation filenames
+        if include_val:
+            val_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC/val")
+            val_df = pd.read_csv(os.path.join(root, "LOC_val_solution.csv"))
+            # Get images containing the desired class
+            filter_fn = lambda x: cls in x
+            filter = val_df["PredictionString"].apply(filter_fn)
+            val_names = val_df["ImageId"][filter]
+            # Get validation filepaths
+            val_paths = [os.path.join(val_dir, val_name + ".JPEG") \
+                for val_name in val_names]
+            self.filepaths.extend(val_paths)
+        # Load images
+        if transform is None:
+            transform = pre_processing.DCGAN_transform()
+        self.images = [transform(default_loader(filepath)) \
+            for filepath in self.filepaths]
+
+    def __len__(self):
+        return len(self.filepaths)
+
+    def __getitem__(self, idx):
+        return self.images[idx]
