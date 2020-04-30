170d40bb519b44e6f9b5e43bb3a5bac73402cae6
Hoang Nghia Tuyen <hnt4499@gmail.com>
2020-04-30 15:13:35 +0800

    Modularize FEMetric for metrics which use feature extractors

--- base/base_metric.py
+++ base/base_metric.py
@@ -1,3 +1,7 @@
+import numpy as np
+import torch
+
+
 class BaseMetric:
     """Base class for any metric implementation. Subclass must implement
     `__str__`, which is used to print useful logs, and `__call__`, which is
@@ -54,3 +58,100 @@ class FPMetric(BaseMetric):
         # Format `self.value` to `dp` decimal places
         num = self.repr.format(self.value)
         return "{}: {}".format(self.name, num)
+
+
+class FEMetric(FPMetric):
+    """Base class for metrics which use features (activations) extracted from
+    a DL model, for example, KID using Inception v3.
+
+    Parameters
+    ----------
+    model
+        An instantiated model defined in
+        `compile.metrics_utils.features_extractors`, which returns the
+        extracted features when called.
+    name : str
+        Name of the metric, for example "kid".
+    max_samples : int
+        Maximum number of samples to use. If None, use all samples.
+    dp : int
+        Number of decimal places to be printed. (default: 4)
+
+    """
+    def __init__(self, model, name, max_samples=None, dp=4):
+        super(FEMetric, self).__init__(name, dp=dp)
+
+        self.model = model
+        self.max_samples = max_samples
+
+    def _get_device(self):
+        self.device = self.trainer.device
+
+    def _get_real_samples(self):
+        """Helper function to get real samples without shuffling"""
+        if not hasattr(self, "real_samples"):
+            self.real_samples = self.trainer.data_loader.dataset.data
+            self.real_samples = torch.stack(self.real_samples)
+        return self.real_samples
+
+    def _get_fake_samples(self):
+        """Helper function to get fake samples without shuffling"""
+        if self.max_samples is None:
+            self.num_samples = len(self.real_samples)
+        else:
+            self.num_samples = min(len(self.real_samples), self.max_samples)
+        # Generate random noise then fake samples
+        self.noise = torch.randn(
+            self.num_samples, self.trainer.length_z, 1, 1,
+            device=self.trainer.device)
+        self.fake_samples = self.trainer.netG(self.noise).detach().cpu()
+        return self.fake_samples
+
+    def _get_features(self, real_samples, fake_samples):
+        """Helper function to extract features from real and fake samples. This
+        function is designed to get called before `_get_shuffled_feats` since
+        we want to share the extracted features among different metrics using
+        the same feature extractor, but we don't want their randomly chosen
+        features to be identical."""
+        # Use `id=0` for real samples because we need to compute only once;
+        # use current batch index as `id` for fake samples
+        self.feats_real = self.model(real_samples, real=True, id=0)
+        self.feats_fake = self.model(
+            fake_samples, real=False, id=self.trainer.current_batch.batch_idx)
+        return self.feats_real, self.feats_fake
+
+    def _get_shuffled_feats(self, feats_real, feats_fake):
+        """Helper function to get randomly shuffled real and fake features,
+        since this type of metric expects input to be in random order"""
+        self.real_idxs = np.random.choice(
+            np.arange(len(feats_real)), size=self.num_samples, replace=False)
+        self.fake_idxs = np.random.choice(
+            np.arange(len(feats_fake)), size=self.num_samples, replace=False)
+        feats_real_shuffled = feats_real[self.real_idxs]
+        feats_fake_shuffled = feats_fake[self.fake_idxs]
+        return feats_real_shuffled, feats_fake_shuffled
+
+    def _calc_score(self, feats_real, feats_fake):
+        """Helper function to calculate score from extracted features"""
+        raise NotImplementedError
+
+    def __call__(self, trainer):
+        with torch.no_grad():
+            # Cache trainer for future use
+            self.trainer = trainer
+            # Get device
+            self._get_device()
+            # Get real samples
+            real_samples = self._get_real_samples()
+            # Get fake samples
+            fake_samples = self._get_fake_samples()
+            # Extract features
+            feats_real, feats_fake = self._get_features(
+                real_samples, fake_samples)
+            # Choose a randomly shuffled subset
+            feats_real, feats_fake = self._get_shuffled_feats(
+                feats_real, feats_fake)
+            # Compute score from extracted features
+            score = self._calc_score(feats_real, feats_fake)
+
+        return score
--- compile/metrics.py
+++ compile/metrics.py
@@ -6,7 +6,7 @@ and return the computed metric.
 import numpy as np
 import torch
 
-from base.base_metric import BaseMetric, FPMetric
+from base.base_metric import FPMetric, FEMetric
 
 from .metrics_utils import calculate_kid
 
@@ -46,7 +46,7 @@ class Accuracy(FPMetric):
         return self.value
 
 
-class KIDScore(FPMetric):
+class KIDScore(FEMetric):
     """Calculate the KID score.
 
     Parameters
@@ -72,76 +72,24 @@ class KIDScore(FPMetric):
         The latest computed KID score (std).
     max_samples
     batch_size
-    subset_size
-    num_subsets
     name
     dp
 
     """
     def __init__(self, model, name, max_samples=None,
                  kid_batch_size=1024, dp=4):
-        super(KIDScore, self).__init__(name, dp)
-        # Feature extraction
-        self.model = model
-        self.max_samples = max_samples
+        super(KIDScore, self).__init__(
+            model=model, name=name, max_samples=max_samples, dp=dp)
         self.kid_batch_size = kid_batch_size
-        # Latest std
-        self.value_std = None
+        self.value_std = None  # latest std
 
     def __str__(self):
         mean = self.repr.format(self.value)
         std = self.repr.format(self.value_std)
         return "{}: {}±{}".format(self.name, mean, std)
 
-    def __call__(self, trainer):
-        with torch.no_grad():
-            # Parse trainer information if needed
-            if self.value is None:
-                self.init(trainer)
-
-            # Generate fake samples. Keep all samples (real and generated)
-            # on CPU for memory efficiency
-            self.noise = torch.randn(
-                self.num_samples, trainer.length_z, 1, 1,
-                device=trainer.device)
-            self.fake_samples = trainer.netG(self.noise).detach().cpu()
-
-            # Extract features, use `id=0` for real samples because we need to
-            # compute only once; use current batch index as `id` for fake
-            # samples
-            self.feats_real = self.model(
-                self.real_samples, real=True, id=0)
-            self.feats_fake = self.model(
-                self.fake_samples, real=False,
-                id=trainer.current_batch.batch_idx)
-
-            # KID expects input to be in random order
-            self.real_idxs = np.random.choice(
-                np.arange(len(self.feats_real)), size=self.num_samples,
-                replace=False)
-            self.fake_idxs = np.random.choice(
-                np.arange(len(self.feats_fake)), size=self.num_samples,
-                replace=False)
-            feats_real = self.feats_real[self.real_idxs]
-            feats_fake = self.feats_fake[self.fake_idxs]
-
-            # Compute KID from features extracted
-            self.value, self.value_std = calculate_kid(
-                feats_fake, feats_real, self.device,
-                batch_size=self.kid_batch_size)
-
+    def _calc_score(self, feats_real, feats_fake):
+        self.value, self.value_std = calculate_kid(
+            feats_real, feats_fake, device=self.device,
+            batch_size=self.kid_batch_size)
         return self.value
-
-    def init(self, trainer):
-        """Parse trainer information, only called once"""
-        # Use the same device as trainer
-        self.device = trainer.device
-        # Get real samples and convert to torch.Tensor
-        self.real_samples = trainer.data_loader.dataset.data
-        self.real_samples = torch.stack(self.real_samples)
-        # Compute number of samples
-        if self.max_samples is None:
-            self.num_samples = len(self.real_samples)
-        else:
-            self.num_samples = min(self.max_samples, len(self.real_samples))
-        self.real_samples = self.real_samples[:self.num_samples]
