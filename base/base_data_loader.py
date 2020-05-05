0c9af8a16c4886d47b566b349c4d28f636baeab2
Hoang Nghia Tuyen <hnt4499@gmail.com>
2020-05-05 22:05:18 +0800

    Simplify DataLoader modules

--- base/base_data_loader.py
+++ base/base_data_loader.py
@@ -1,22 +1,37 @@
-import numpy as np
 from torch.utils.data import DataLoader
 from torch.utils.data.dataloader import default_collate
-from torch.utils.data.sampler import SubsetRandomSampler
 
 
 class BaseDataLoader(DataLoader):
     """
-    Base class for all data loaders
+    Base class for all data loaders, where `sampler` is set to None by default.
     """
     def __init__(self, dataset, batch_size, shuffle, num_workers):
-        self.shuffle = shuffle
+        """Initialize the data loader.
 
-        self.batch_idx = 0
+        Parameters
+        ----------
+        dataset
+            A dataset, instantiated object of a class defined in
+            `data_loaders.datasets`.
+        batch_size : int
+            Mini-batch size for training.
+        shuffle : bool
+            Whether to shuffle the training set.
+        num_workers : int
+            The number of subprocesses to use for data loading. `0` means that
+            the data will be loaded in the main process.
+
+        Attributes
+        ----------
+
+
+        """
         self.n_samples = len(dataset)
 
         self.init_kwargs = {
             "dataset": dataset, "batch_size": batch_size,
-            "shuffle": self.shuffle, "collate_fn": default_collate,
+            "shuffle": shuffle, "collate_fn": default_collate,
             "num_workers": num_workers
         }
         super().__init__(sampler=None, **self.init_kwargs)
--- data_loaders/data_loaders.py
+++ data_loaders/data_loaders.py
@@ -2,46 +2,12 @@ from base import BaseDataLoader
 
 
 class ImageNetLoader(BaseDataLoader):
-    """ImageNet data loader.
-
-    Parameters
-    ----------
-    dataset
-        A dataset, instantiated object of a class defined in
-        `data_loaders.datasets`.
-    batch_size : int
-        Mini-batch size for training.
-    shuffle : bool
-        Whether to shuffle the training set.
-    num_workers : int
-        The number of subprocesses to use for data loading. `0` means that the
-        data will be loaded in the main process. (default: `0`)
-
     """
-    def __init__(self, dataset, batch_size=128, shuffle=True, num_workers=1):
-        self.dataset = dataset
-        super().__init__(
-            self.dataset, batch_size, shuffle, num_workers=num_workers)
+    ImageNet data loader.
+    """
 
 
 class CelebALoader(BaseDataLoader):
-    """CelebA data loader.
-
-    Parameters
-    ----------
-    dataset
-        A dataset, instantiated object of a class defined in
-        `data_loaders.datasets`.
-    batch_size : int
-        Mini-batch size for training.
-    shuffle : bool
-        Whether to shuffle the training set.
-    num_workers : int
-        The number of subprocesses to use for data loading. `0` means that the
-        data will be loaded in the main process. (default: `0`)
-
     """
-    def __init__(self, dataset, batch_size=128, shuffle=True, num_workers=1):
-        self.dataset = dataset
-        super().__init__(
-            self.dataset, batch_size, shuffle, num_workers=num_workers)
+    CelebA data loader.
+    """
